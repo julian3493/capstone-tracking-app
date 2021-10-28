0d456b2642877d29dd592980397cd1ea7b7ef836
julian3493 <julianramosarevalo@gmail.com>
2021-10-28 18:50:27 -0500

    create tracks reducer and show user tracks in Tracks component

--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -1,27 +1,70 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
 import Logout from './Logout';
 import Navigation from './Navigation';
+import { userTracks } from '../redux/actions/actions';
 
 const Tracks = () => {
+  const dispatch = useDispatch();
+
   const fetchData = async () => {
     await axios.get('http://localhost:3001/tracks',
       { withCredentials: true }).then((response) => {
-      console.log(response);
+      if (response.data.registers) {
+        const tracks = response.data.registers;
+        dispatch(userTracks(tracks));
+      }
     }).catch((err) => {
       console.log('resgistration error', err);
     });
   };
 
-  const handleCLick = (e) => {
-    fetchData();
-    e.preventDefault();
+  const showTracks = () => {
+    const trackStored = useSelector((state) => state.tracks);
+    if (trackStored && trackStored.payload.length > 0) {
+      return trackStored.payload.map((track) => {
+        const {
+          day, electrodomestic, id, watts,
+        } = track;
+        const timeConnected = track.time_connected;
+        return (
+          <div key={id}>
+            <p>
+              Date:
+              {day}
+            </p>
+            <p>
+              Electrodomestic:
+              {electrodomestic}
+            </p>
+            <p>
+              Time Connected:
+              {timeConnected}
+            </p>
+            <p>
+              Watts:
+              {watts}
+            </p>
+          </div>
+        );
+      });
+    }
+    return (
+      <div>
+        <p> </p>
+      </div>
+    );
   };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div>
       <h1>Tracks</h1>
-      <button type="button" onClick={handleCLick}>fetch</button>
+      {showTracks()}
       <Navigation />
       <Logout />
     </div>
