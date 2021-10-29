d1b4abd6e3c22ae9f6a4470ab83d474633f226ab
julian3493 <julianramosarevalo@gmail.com>
2021-10-29 17:25:03 -0500

    add style to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import TopBar from './TopBar';
+import '../styles/Home.css';
 
 const Home = () => (
   <div>
     {TopBar('App Tracker')}
-    <div>
-      <Link to="/login">Login</Link>
-      <Link to="/registration">Create Account</Link>
+    <div className="options">
+      <button type="button"><Link to="/login">Login</Link></button>
+      <button type="button"><Link to="/registration">Create Account</Link></button>
     </div>
   </div>
 );
